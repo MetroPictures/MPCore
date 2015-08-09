dea6cd6878f52f2d5aae1a577df94092eb2b6669
harlo <harlo.holmes@gmail.com>
2015-08-09 12:10:58 +0000

    setup scripts again

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 MODULE_DIR=$1
 REDIS_PORT=$2
 
+# install the basics
+sudo apt-get update
+sudo apt-get install -y python-dev python-pip
+sudo pip install virtualenv
+
 # setup redis
 cd ~
 wget http://download.redis.io/redis-stable.tar.gz
@@ -16,9 +21,15 @@ sudo mkdir /etc/redis
 sudo mkdir -p /var/redis/$REDIS_PORT
 sudo cp utils/redis_init_script /etc/init.d/redis_$REDIS_PORT
 
+# make virtualenv
+virtualenv .venv
+source .venv/bin/activate
+
 # pip install
 cd $MODULE_DIR/core/MPy
 pip install -r requirements.txt
+
+deactivate .venv
 
 # symlink all media
 cd $MODULE_DIR/core/media
