07ed22bdb3445e6c2c1d725708159c9eb90a83ee
harlo <harlo.holmes@gmail.com>
2015-08-16 19:58:02 -0400

    initial stubs for video

---
+++ video_pad.py
@@ -0,0 +1,23 @@
+import pygame, logging
+from time import time, sleep
+
+class MPVideoPad():
+	def __init__(self):
+		logging.basicConfig(filename=self.conf['d_files']['vp']['log'], level=logging.DEBUG)
+
+	def start_video_pad(self):
+		pygame.init()
+		clock = pygame.time.Clock()
+
+		self.vp_setup()
+		logging.info("VideoPad launched.")
+
+		while True:
+			self.vp_draw()
+			clock.tick(60)
+		
+	def vp_setup(self): 
+		self.screen = pygame.display.set_mode(self.conf['screen_size'])
+
+	def vp_draw(self):
+		pygame.display.flip()
