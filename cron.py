ca4b1afb08be0cbde49124889426c48dd815b98b
harlo <harlo.holmes@gmail.com>
2015-11-08 18:37:06 +0000

    triggering log truncate on shutdown

--- cron.py
+++ cron.py
@@ -5,9 +5,8 @@ def clear_logs():
 	for r, _, files in os.walk(os.path.join(os.getcwd(), ".monitor")):
 		for log in [f for f in files if re.match(r'.*\.log\.txt', f)]:
 			with open(os.path.join(r, log), 'wb+') as L:
-				print r, log
 				L.truncate()
-				L.write("Log last truncated at %s" % ctime())
+				L.write("Log last truncated at %s\n\n" % ctime())
 
 		break
 
